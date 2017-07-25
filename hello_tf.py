c03e51ce802716f76b4e02dee0ee6ba08c3ee219
jshn232 <jshn232@163.com>
2017-07-25 17:15:41 +0800

    update

--- hello_tf.py
+++ hello_tf.py
@@ -11,7 +11,7 @@ print(sess.run(hello))
 
 a = tf.constant(100)
 b = tf.constant(200)
-
+print('a+b')
 print(sess.run(a+b))
 
 
@@ -20,6 +20,16 @@ max2 = tf.constant([[2.,-4.],[5.,-2.],[2.,17]])
 
 product = tf.matmul(max1,max2)
 
+product1 = sess.run(product)
+print('sess run product')
 print(sess.run(product))
+
+print('product1')
+print(product1)
+
+max3 = tf.constant([[2.,3.],[-1.,1.]])
+
+print('max3')
+print(sess.run(tf.matmul(product1,max3)))
 
 sess.close()
